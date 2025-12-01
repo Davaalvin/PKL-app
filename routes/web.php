bd5acc1c5bb7c4190f1ae8a134e78e125deb25f0
richoyouknow <m.richo.saputra69@gmail.com>
2025-12-01 20:33:41 +0700

    tamplate

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,6 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get("/app", function () {
-    return view("layouts.app");
-});
+Route::get('/', function () {
+    return view('layout.master', ['title' => 'Dashboard']);
+})->name('master');
